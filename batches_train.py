2efebffe72e4d6c4f20e7b84dcd5742e526941f7
ADITYA SINGH <singhaditya08aug@gmail.com>
2018-06-06 20:01:14 +0530

    generalized batches code

--- batches_train.py
+++ batches_train.py
@@ -1,10 +1,17 @@
 ## to train nn on large images 
+
+##hyperparameters to be tunes according to the dataset
+
+no_of_epochs = 2
+image_height = 500
+image_width = 500
+no_of_small_batches = 130
+
+## actual code
 epoch = 0
-
-
-while epoch <2:
+while epoch <no_of_epochs:
   i =0
-  while i<130:
+  while i<no_of_small_batches:
     k = 0
     X_train = []
     print('epoch : ',epoch)
@@ -12,22 +19,18 @@ while epoch <2:
     while k<100:
       name = 'drive/Colab Notebooks/train_images/Img-' + str(i*100 + k+1) +'.jpg'
       img = cv2.imread(name)
-      img = cv2.resize(img, (500,500))
+      img = cv2.resize(img, (image_height, image_width))
       X_train.append(img)
       k = k + 1
     print('loading 100 images done')
     X_train_ = np.asarray(X_train)
     y_train_ = y_train[i*100 : (i+1)*100]
   
-    print(i*100 + k+1)
-  
-    print(i*100)
-    print((i+1)*100)
-    
-    checkpoint = ModelCheckpoint(filepath, monitor='acc', verbose=1, save_best_only=True, mode='max')
+    #fitting the model 
     model.fit_generator(gen.flow(X_train_, y_train_, batch_size = 50),
                     steps_per_epoch=len(X_train) /50,epochs=1,verbose = 1,
                     shuffle = True)
+    # saving the model
     model.save('resnet_500.h5')
     i = i + 1
   epoch = epoch + 1
