1c3b1290edb1e655b2fb57c89ffca6a519c38a5f
ADITYA SINGH <singhaditya08aug@gmail.com>
2018-06-06 20:06:19 +0530

    made few corrections

--- batches_train.py
+++ batches_train.py
@@ -1,11 +1,16 @@
+## author -ADITYA SINGH
+
 ## to train nn on large images 
 
-##hyperparameters to be tunes according to the dataset
+##hyperparameters to be tuned according to the dataset
 
 no_of_epochs = 2
 image_height = 500
 image_width = 500
 no_of_small_batches = 130
+no_of_images = 100
+
+
 
 ## actual code
 epoch = 0
@@ -15,16 +20,18 @@ while epoch <no_of_epochs:
     k = 0
     X_train = []
     print('epoch : ',epoch)
-    print('i : ', i)
-    while k<100:
-      name = 'drive/Colab Notebooks/train_images/Img-' + str(i*100 + k+1) +'.jpg'
+    print('no of mini batches  : ', i, ' out of : ', no_of_small_batches)
+    while k<no_of_images:
+      name = 'drive/Colab Notebooks/train_images/Img-' + str(i*no_of_images + k+1) +'.jpg'
       img = cv2.imread(name)
       img = cv2.resize(img, (image_height, image_width))
       X_train.append(img)
       k = k + 1
     print('loading 100 images done')
+    # converting the training set into an numpy array
     X_train_ = np.asarray(X_train)
-    y_train_ = y_train[i*100 : (i+1)*100]
+    # extracting the sepecif portion of y_train 
+    y_train_ = y_train[i*no_of_images : (i+1)*no_of_images]
   
     #fitting the model 
     model.fit_generator(gen.flow(X_train_, y_train_, batch_size = 50),
@@ -33,4 +40,5 @@ while epoch <no_of_epochs:
     # saving the model
     model.save('resnet_500.h5')
     i = i + 1
+  # incrementing the loop counter
   epoch = epoch + 1
